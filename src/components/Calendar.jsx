a36d4de7bfd47f1e7a23adb7fbbb056fd85ea53e
SofiDev <angiemeow89@gmail.com>
2025-01-02 02:23:30 -0600

    ♻️ Se agrega current Year dinamico

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
 import { Tooltip as MuiTooltip } from '@mui/material';
 import GitHubCalendar from 'react-github-calendar';
-import YearButton from './components/YearButton';
+import YearButton from '../components/YearButton';
 
-import './App.css';
-
-function App() {
+const Calendar = () => {
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
   const [activeButton, setActiveButton] = useState(new Date().getFullYear());
-
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
   const handleYearButtonClick = (year) => {
     setSelectedYear(year);
     setActiveButton(year);
@@ -16,37 +14,37 @@ function App() {
 
   return (
     <>
-
-
       <div className="calendar__container">
         <GitHubCalendar
-          username="Sofidev"
+          username="SofiDevO"
           year={selectedYear}
           weekStart={1}
           theme={{
-            light: ['hsl(0, 0%, 92%)', 'rebeccapurple'],
-            dark: ['hsl(0, 0%, 22%)', 'hsla(276, 100%, 45%, 1)'],
+            light: ["hsl(0, 0%, 92%)", "rebeccapurple"],
+            dark: ["hsl(0, 0%, 22%)", "hsla(276, 100%, 45%, 1)"],
           }}
           renderBlock={(block, activity) => (
-            <MuiTooltip title={`${activity.count} activities on ${activity.date}`}>
+            <MuiTooltip
+              title={`${activity.count} activities on ${activity.date}`}
+            >
               {block}
             </MuiTooltip>
           )}
         />
       </div>
       <div className="year__buttons">
-        {[2024, 2023, 2022].map((year) => (
+      {[currentYear , currentYear -1, currentYear -2].map((year) => (
           <YearButton
             key={year}
             year={year}
             currentYear={selectedYear}
             onClick={() => handleYearButtonClick(year)}
-            className={activeButton === year ? 'is-active' : ''}
+            className={activeButton === year ? "is-active" : ""}
           />
         ))}
       </div>
     </>
   );
-}
+};
 
-export default App;
+export default Calendar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import "./App.css"
 import './index.css'
+import Calendar from './components/Calendar'
+
+
+
+
+function App(){
+  return(
+      <Calendar />
+      
+  )
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
